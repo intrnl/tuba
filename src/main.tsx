c80fa827abdb3118a9ae4d5b8bd20e7af72ada2e
intrnl <intrnl28+git@gmail.com>
2023-06-18 16:35:17 +0700

    feat: theming

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { lazy } from 'solid-js';
+import { createRenderEffect, createRoot, lazy } from 'solid-js';
 import { render } from 'solid-js/web';
 
 import { Router, useRoutes } from '@solidjs/router';
 
 import routes from '~/router-routes.ts';
+import { useMediaQuery } from '~/utils/media-query.ts';
 
 import '~/styles/tailwind.css';
 
@@ -22,5 +23,30 @@ const App = () => {
 		</Router>
 	);
 };
+
+createRoot(() => {
+	createRenderEffect((prev: string | undefined) => {
+		// @todo implement proper theming
+		const theme = 'auto';
+
+		if (prev === theme) {
+			return theme;
+		}
+
+		const cl = document.documentElement.classList;
+
+		if (theme === 'auto') {
+			const isDark = useMediaQuery('(prefers-color-scheme: dark)');
+
+			createRenderEffect(() => {
+				cl.toggle('dark', isDark());
+			});
+		} else {
+			cl.toggle('dark', theme === 'dark');
+		}
+
+		return theme;
+	});
+});
 
 render(() => <App />, document.body);
